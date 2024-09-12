dab663d581c8abc6fbd3aec314fe578f5dc3b27f
Chaya-nallani <ch.chaya@gmail.com>
2024-09-11 20:10:48 -0400

    Update TestNgRunner.java

--- src/test/java/cucumberoptions/TestNgRunner.java
+++ src/test/java/cucumberoptions/TestNgRunner.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources", glue = "StepDefination", monochrome = true, tags ="@StackTest or @negative or @SmokeTest or @Monkey or @LinkedTest or @TreesTest or @QueuesTest or @GraphsTest", plugin = {
+@CucumberOptions(features = "src/test/resources", glue = "StepDefination", monochrome = true, tags ="@StackTest or @negative", plugin = {
 		"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "html:target/cucumber.html",
 		"json:target/cucumber.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
 public class TestNgRunner extends AbstractTestNGCucumberTests {
